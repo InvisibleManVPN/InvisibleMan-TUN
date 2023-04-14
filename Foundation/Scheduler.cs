175d549ce943509b72fc0cf2cec463359a5d19e4
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-04-14 12:25:38 +0330

    Add scheduler

---
+++ Foundation/Scheduler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+
+namespace InvisibleManTUN.Foundation
+{
+    public class Scheduler
+    {
+        public void WaitUntil(Func<bool> condition, int millisecondsTimeout, string timeoutError)
+        {
+            bool isConditionSatisfied = false;
+            int timeRate = millisecondsTimeout / 20;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(timeRate);
+
+                if (condition.Invoke())
+                {
+                    isConditionSatisfied = true;
+                    break;
+                }
+            }
+
+            if (!isConditionSatisfied)
+            {
+                Console.WriteLine(timeoutError);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
