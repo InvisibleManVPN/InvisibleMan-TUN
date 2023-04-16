48ea09b415f1c8758eeaca65d8ada62d11c52722
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-04-17 02:15:25 +0330

    Add csharp tun wrapper

---
+++ Core/TunWrapper.cs
@@ -0,0 +1,49 @@
+using System.Runtime.InteropServices;
+
+namespace InvisibleManTUN.Core
+{
+    using Values;
+
+    internal class TunWrapper
+    {
+        public static void StartTunnel(string device, string proxy)
+        {
+            StartTunnel(device, proxy);
+
+            [DllImport(Path.TUN_DLL, EntryPoint = "StartTunnel")]
+            static extern void StartTunnel(string device, string proxy);
+        }
+
+        public static void StopTunnel()
+        {
+            StopTunnel();
+
+            [DllImport(Path.TUN_DLL, EntryPoint = "StopTunnel")]
+            static extern void StopTunnel();
+        }
+
+        public static void SetInterfaceAddress(string device, string address)
+        {
+            SetInterfaceAddress(device, address);
+
+            [DllImport(Path.TUN_DLL, EntryPoint = "SetInterfaceAddress")]
+            static extern void SetInterfaceAddress(string device, string address);
+        }
+
+        public static void SetInterfaceDns(string device, string dns)
+        {
+            SetInterfaceDns(device, dns);
+
+            [DllImport(Path.TUN_DLL, EntryPoint = "SetInterfaceDns")]
+            static extern void SetInterfaceDns(string device, string dns);
+        }
+
+        public static void SetRoutes(string server, string address, string gateway, int index)
+        {
+            SetRoutes(server, address, gateway, index);
+
+            [DllImport(Path.TUN_DLL, EntryPoint = "SetRoutes")]
+            static extern void SetRoutes(string server, string address, string gateway, int index);
+        }
+    }
+}
--- Values/Path.cs
+++ Values/Path.cs
@@ -2,9 +2,7 @@ namespace InvisibleManTUN.Values
 {
     public static class Path
     {
-        public const string TUN2SOCKS_EXE = "tun2socks.exe";
-        public const string NETSH_EXE = "netsh.exe";
-        public const string CMD_EXE = "cmd.exe";
+        public const string TUN_DLL = "tun.dll";
         public const string IPHLPAPI_DLL = "iphlpapi.dll";
     }
 }
