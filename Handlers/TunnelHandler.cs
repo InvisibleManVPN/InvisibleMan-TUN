41ed32e56e0b01bba77577057ef238ab25371080
invisiblemanvpn <invisiblemanvpn@gmail.com>
2023-04-17 02:37:16 +0330

    Register and setup core and refactor tunnel handler Refactor service manager and remove processor, tun2socks and network classes

--- Handlers/TunnelHandler.cs
+++ Handlers/TunnelHandler.cs
@@ -1,27 +1,111 @@
+using System;
+using System.Threading.Tasks;
+
 namespace InvisibleManTUN.Handlers
 {
-    using Tunnel;
+    using Foundation;
+    using Utilities;
 
     public class TunnelHandler : Handler
     {
-        private Tun2Socks tun2Socks;
-        private Network network;
+        private Scheduler scheduler;
+
+        private Action onStopTunnel;
+        private Action<string, string> onStartTunnel;
+        private Action<string, string> onSetInterfaceAddress;
+        private Action<string, string> onSetInterfaceDns;
+        private Action<string, string, string, int> onSetRoutes;
 
         public TunnelHandler()
         {
-            this.tun2Socks = new Tun2Socks();
-            this.network = new Network();
+            this.scheduler = new Scheduler();
+        }
+
+        public void Setup(
+            Action onStopTunnel,
+            Action<string, string> onStartTunnel,
+            Action<string, string> onSetInterfaceAddress,
+            Action<string, string> onSetInterfaceDns,
+            Action<string, string, string, int> onSetRoutes
+        )
+        {
+            this.onStopTunnel = onStopTunnel;
+            this.onStartTunnel = onStartTunnel;
+            this.onSetInterfaceAddress = onSetInterfaceAddress;
+            this.onSetInterfaceDns = onSetInterfaceDns;
+            this.onSetRoutes = onSetRoutes;
         }
 
         public void Start(string device, string proxy, string address, string server, string dns)
         {
-            tun2Socks.RunProcess(device, proxy);
-            network.RunProcess(device, address, server, dns);
+            try
+            {
+                StartTunnel();
+                WaitUntilInterfaceCreated();
+                SetInterfaceAddress();
+                WaitUntilInterfaceAddressSet();
+                SetInterfaceDns();
+                SetRoutes();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            void StartTunnel()
+            {
+                new Task(() => {
+                    onStartTunnel.Invoke(device, proxy);
+                }).Start();
+            }
+
+            void WaitUntilInterfaceCreated()
+            {
+                scheduler.WaitUntil(
+                    condition: IsInterfaceExists,
+                    millisecondsTimeout: 6000,
+                    $"Device with the name '{device}' was not found."
+                );
+            }
+
+            void SetInterfaceAddress()
+            {
+                onSetInterfaceAddress.Invoke(device, address);
+            }
+
+            void WaitUntilInterfaceAddressSet()
+            {
+                scheduler.WaitUntil(
+                    condition: IsInterfaceAddressWasSet,
+                    millisecondsTimeout: 6000,
+                    $"'{address}' was not set to '{device}' device."
+                );
+            }
+
+            void SetInterfaceDns()
+            {
+                onSetInterfaceDns.Invoke(device, dns);
+            }
+
+            void SetRoutes()
+            {
+                onSetRoutes.Invoke(
+                    server, 
+                    address, 
+                    NetworkUtility.GetDefaultGateway(address), 
+                    NetworkUtility.GetNetworkInterfaceIndex(device)
+                );
+            }
+
+            bool IsInterfaceExists() => NetworkUtility.IsInterfaceExists(device);
+
+            bool IsInterfaceAddressWasSet() => NetworkUtility.IsInterfaceAddressWasSet(device, address);
         }
 
         public void Stop()
         {
-            tun2Socks.StopProcess();
+            onStopTunnel.Invoke();
         }
     }
 }
